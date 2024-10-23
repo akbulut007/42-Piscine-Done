5d9ab342d9b9c394694f80afd2752d68d7483827
akbulut007 <103904132+akbulut007@users.noreply.github.com>
2024-10-23 17:22:03 +0200

    C00-C02 intmain
    C00-C02 intmain

---
+++ C00_intmain.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intmain.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhakbul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 15:33:38 by muhakbul          #+#    #+#             */
+/*   Updated: 2024/10/05 16:13:03 by muhakbul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*int main()
+{
+	ft_putchar('m');
+	return(0);
+}
+
+int main()
+{
+	ft_print_alphabet();
+	return(0);
+}
+
+int main()
+{
+	ft_print_reverse_alphabet();
+	return(0);
+}
+
+int main(void)
+{
+	ft_print_numbers();
+}
+
+int main()
+{
+	ft_is_negative(-1);
+}
+
+int main(void)
+{
+    ft_print_comb();
+    return 0;
+}
+/
