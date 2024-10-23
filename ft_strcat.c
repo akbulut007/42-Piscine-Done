46cdc5822b132685652f3f2c1b8b0ed0c9334aba
akbulut007 <103904132+akbulut007@users.noreply.github.com>
2024-10-23 17:09:07 +0200

    C03
    C03

---
+++ ft_strcat.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhakbul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 16:22:14 by muhakbul          #+#    #+#             */
+/*   Updated: 2024/10/14 18:50:47 by muhakbul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*int main()
+{
+	char src[]="Merhabalar";
+	char dest[]="123456";
+
+	printf("%s", ft_strcat(src, dest));
+}*/
---
+++ ft_strstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhakbul <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 16:23:04 by muhakbul          #+#    #+#             */
+/*   Updated: 2024/10/14 18:47:44 by muhakbul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (to_find[j] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		while (str[i + j] == to_find[j] && str[i + j] != '\0')
+			j++;
+		if (to_find[j] == '\0')
+			return (str + i);
+		i++;
+		j = 0;
+	}
+	return (0);
+}
+/*int main()
+{
+	char str[]="42 1337 Network 2021 piscine Benguerir Khouribga";
+	char find[]="piscine";
+
+	printf("%s", ft_strstr(str, find));
+}*/
