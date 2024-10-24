29c880f3d8902b74e330ecdba66d51c1447c5b77
akbulut007 <103904132+akbulut007@users.noreply.github.com>
2024-10-24 19:58:21 +0200

    exam questions
    exam questions

--- first_word.c
+++ first_word.c
@@ -1,25 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   putstr.c                                           :+:      :+:    :+:   */
+/*   first_word.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: muhakbul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/23 18:49:32 by muhakbul          #+#    #+#             */
-/*   Updated: 2024/10/23 18:49:34 by muhakbul         ###   ########.fr       */
+/*   Created: 2024/10/24 15:41:49 by muhakbul          #+#    #+#             */
+/*   Updated: 2024/10/24 15:41:50 by muhakbul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+void	ft_putchar(char c)
 {
-	int i = 0;
-	
-	while(str[i] != '\0')
-	{
-		write(1, &str[i], 1);
-	i++;
-	}
+	write(1, &c, 1);
 }
 
+void	ft_first_word(char *str)
+{
+	int i;
+
+	i = 0;
+    
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+		i++;
+  
+	while (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		ft_first_word(av[1]);
+		return (0);
+	}
+	else 
+		ft_putchar('\n');
+	return (0);
+}
--- rotonee.c
+++ rotonee.c
@@ -1,25 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   putstr.c                                           :+:      :+:    :+:   */
+/*   rotonee.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: muhakbul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/23 18:49:32 by muhakbul          #+#    #+#             */
-/*   Updated: 2024/10/23 18:49:34 by muhakbul         ###   ########.fr       */
+/*   Created: 2024/10/24 17:43:07 by muhakbul          #+#    #+#             */
+/*   Updated: 2024/10/24 18:14:04 by muhakbul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+void	rotonee(char *str)
 {
-	int i = 0;
-	
-	while(str[i] != '\0')
+	int c;
+
+	c = 0;
+
+	while(str[c])
 	{
-		write(1, &str[i], 1);
-	i++;
+		if(str[c] >= 'a' && str[c] <= 'y')
+			str[c] = str[c] + 1;
+		else if(str[c] >= 'A' && str[c] <= 'Y')
+			str[c] = str[c] + 1;
+		else if(str[c] == 'z')
+			str[c] = 'a';
+		else if(str[c] == 'Z')
+			str[c] = 'A';
+		write(1, &str[c], 1);
+		c++;
 	}
 }
-
+int main(int ac, char **av)
+{
+	if(ac == 2)
+		rotonee(av[1]);
+	write(1, "\n", 1);
+	return(0);
+}
